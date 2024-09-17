c5b17298fba243940f491b827e1168688056c628
Raghu <you@example.comraghuvamsi1819@gmail.com>
2024-09-17 09:15:25 +0530

    first commit

---
+++ app/src/main/java/com/eoshopping/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.eoshopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        var navController = Navigation.findNavController(this,R.id.host_fragment )
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+    }
+}
